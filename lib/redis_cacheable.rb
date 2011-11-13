23d4d2b0bfd9aa8d7080dccb6e5d85acd99d96c0
Trevor Rosen <trevor@catapult-creative.com>
2011-11-13 17:02:48 -0600

    Use ActiveSupport for now

--- lib/redis_cacheable.rb
+++ lib/redis_cacheable.rb
@@ -8,4 +8,11 @@ require "redis-namespace" # The Redis gem is dependency of redis-namespace
 module RedisCacheable
   class NonConvertableClassError < Exception; end
   class Config < OpenStruct; end
+
+  class Util
+    def self.underscore(base)
+      ActiveSupport::Inflector.underscore(base)
+    end
+  end
+
 end
